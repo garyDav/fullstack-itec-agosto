6df9bf9a38221fb01292edea78f1332b7c901a21
garyDav <gary.2810.dav@gmail.com>
2025-09-24 21:22:51 -0400

    use client, useState

--- nextjs/02-my-dashboard/src/app/dashboard/counter/page.tsx
+++ nextjs/02-my-dashboard/src/app/dashboard/counter/page.tsx
@@ -1,4 +1,20 @@
+import { Metadata } from "next"
+
+import { CartCounter } from "@/shopping-cart"
+
+export const metadata: Metadata = {
+  title: 'Counter Page',
+  description: 'Este es un simple contador'
+}
 
 export default function CounterPage() {
-  return <h1>Counter Page</h1>
+
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+
+      <CartCounter />
+
+    </div>
+  )
 }
